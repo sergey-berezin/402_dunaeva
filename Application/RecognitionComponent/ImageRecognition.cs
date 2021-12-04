ad9dd58509b95313c665c13b0e03dc4e050a7258
nastyadunaeva <anastasya.dunaeva@gmail.com>
2021-12-04 15:34:22 +0300

    web server

--- Application/RecognitionComponent/ImageRecognition.cs
+++ Application/RecognitionComponent/ImageRecognition.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using static Microsoft.ML.Transforms.Image.ImageResizingEstimator;
 using System.Collections.Concurrent;
 using System.Threading;
-
+using System.Linq;
 
 namespace RecognitionComponent
 {
@@ -128,6 +128,60 @@ namespace RecognitionComponent
 
             sw.Stop();
             Console.WriteLine($"Done in {sw.ElapsedMilliseconds}ms.");
+        }
+
+        public static IReadOnlyList<YoloV4Result> SingleImageRecognize(Bitmap bitmap, CancellationToken token)
+        {
+            if (token.IsCancellationRequested) return new List<YoloV4Result>();
+
+            var yoloV4Results = new ConcurrentStack<YoloV4Result>();
+            MLContext mlContext = new MLContext();
+
+            var pipeline = mlContext.Transforms.ResizeImages(inputColumnName: "bitmap", outputColumnName: "input_1:0", imageWidth: 416, imageHeight: 416, resizing: ResizingKind.IsoPad)
+                .Append(mlContext.Transforms.ExtractPixels(outputColumnName: "input_1:0", scaleImage: 1f / 255f, interleavePixelColors: true))
+                .Append(mlContext.Transforms.ApplyOnnxModel(
+                    shapeDictionary: new Dictionary<string, int[]>()
+                    {
+                        { "input_1:0", new[] { 1, 416, 416, 3 } },
+                        { "Identity:0", new[] { 1, 52, 52, 3, 85 } },
+                        { "Identity_1:0", new[] { 1, 26, 26, 3, 85 } },
+                        { "Identity_2:0", new[] { 1, 13, 13, 3, 85 } },
+                    },
+                    inputColumnNames: new[]
+                    {
+                        "input_1:0"
+                    },
+                    outputColumnNames: new[]
+                    {
+                        "Identity:0",
+                        "Identity_1:0",
+                        "Identity_2:0"
+                    },
+                    modelFile: modelPath, recursionLimit: 100));
+
+            var model = pipeline.Fit(mlContext.Data.LoadFromEnumerable(new List<YoloV4BitmapData>()));
+
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<YoloV4BitmapData, YoloV4Prediction>(model);
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var predict = predictionEngine.Predict(new YoloV4BitmapData() { Image = bitmap });
+
+            var results = predict.GetResults(classesNames, "", 0.3f, 0.7f);
+
+            foreach (var res in results)
+            {
+                var x1 = res.BBox[0];
+                var y1 = res.BBox[1];
+                var x2 = res.BBox[2];
+                var y2 = res.BBox[3];
+                Console.WriteLine($"{res.Label} in a rectangle between ({x1:0.0}, {y1:0.0}) and ({x2:0.0}, {y2:0.0}) with probability {res.Confidence.ToString("0.00")}");
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Done in {sw.ElapsedMilliseconds}ms.");
+            return results;
         }
     }
 }
---
+++ Application/Web/Controllers/ImageController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RecognitionComponent;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Drawing;
+using RecognitionComponent;
+using System.IO;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("/api/images")]
+    public class ImageController : ControllerBase
+    {
+        private ImageContext db;
+        public ImageController(ImageContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public ImageEntity[] GetImages()
+        {
+            return db.GetAllImages().ToArray();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ImageEntity> GetImage(string id)
+        {
+            int idx = Int32.Parse(id);
+            ImageEntity imageEntity = db.TryGetImage(idx);
+            if (imageEntity != null)
+            {
+                return imageEntity;
+            }
+            else
+            {
+                return StatusCode(404, "Image with given id is not found");
+            }
+        }
+
+        [HttpGet("hash/{hash}")]
+        public ActionResult<ImageEntity[]> GetImageByHash(string hash)
+        {
+            int h = Int32.Parse(hash);
+            var imageEntity = db.TryGetImageHash(h);
+            if (imageEntity != null)
+            {
+                return imageEntity.ToArray();
+            }
+            else
+            {
+                return StatusCode(404, "Image with given hash code is not found");
+            }
+        }
+
+        [HttpGet("results/{imageEntityId}")]
+        public ActionResult<ResultEntity[]> GetResultsByImageEntityId(string imageEntityId)
+        {
+            int idx = Int32.Parse(imageEntityId);
+            return db.GetResultsByImageEntityId(idx);
+        }
+
+        [HttpGet("box/{resultEntityId}")]
+        public ActionResult<BBox> GetBoxByResultEntityId(string resultEntityId)
+        {
+            int idx = Int32.Parse(resultEntityId);
+            return db.GetBoxByResultEntityId(idx);
+        }
+
+        [HttpPost("recognize")]
+        public ActionResult<YoloV4Result[]> PostImage([FromBody] byte[] bytes)
+        {
+            bool isExist = false;
+            int hash = ImageEntity.ComputeHashCode(bytes);
+            var sameHash = db.TryGetImageHash(hash);
+            if (sameHash != null)
+            {
+                foreach (var entity in sameHash)
+                {
+                    if (Enumerable.SequenceEqual(entity.Image, bytes))
+                    {
+                        isExist = true;
+                    }
+                }
+            }
+            var ms = new MemoryStream(bytes);
+            Bitmap bitmap = new Bitmap(ms);
+            var results = ImageRecognition.SingleImageRecognize(bitmap, new System.Threading.CancellationToken());
+            if (isExist)
+            {
+                return StatusCode(200, results.ToArray());
+            }
+            var imageEntity = new ImageEntity();
+            if (results == null)
+            {
+                return StatusCode(201, new List<YoloV4Result>());
+            }
+            foreach (var result in results)
+            {
+                BBox box = new BBox()
+                {
+                    X1 = result.BBox[0],
+                    Y1 = result.BBox[1],
+                    X2 = result.BBox[2],
+                    Y2 = result.BBox[3]
+                };
+                ResultEntity resultEntity = new ResultEntity()
+                {
+                    Confidence = result.Confidence,
+                    Label = result.Label
+                };
+                box.ResultEntity = resultEntity;
+                resultEntity.BBox = box;
+                imageEntity.Image = bytes;
+                imageEntity.HashCode = hash;
+                imageEntity.Results.Add(resultEntity);
+                db.AddNewBox(box);
+                db.AddNewResult(resultEntity);
+            }
+            db.AddNewImageEntity(imageEntity);
+            return StatusCode(201, results.ToArray());
+        }
+
+        [HttpDelete("clean")]
+        public ActionResult Remove()
+        {
+            db.Remove();
+            return StatusCode(204, "successfully removed");
+        }
+
+
+    }
+}
